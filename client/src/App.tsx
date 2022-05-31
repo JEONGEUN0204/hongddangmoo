90128251c7a5e1f561f072b89a2d6d56679fe8f3
jeongeun <wjddms9921@gmail.com>
2022-06-01 04:02:23 +0900

    init

---
+++ client/src/App.tsx
@@ -0,0 +1,11 @@
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      홍당무 마켓
+    </div>
+  );
+}
+
+export default App;
---
+++ server/app.ts
@@ -0,0 +1,24 @@
+import express from 'express';
+
+const app = express();
+
+app.use(express.static('build')); //build폴더 안에 있는 파일 마음대로 꺼내쓸 수 있음
+app.get('/', function (req, res) {
+  res.sendFile(__dirname+'/build/index.html');
+})
+
+app.listen(3000,()=>{
+    console.log('server is runnig!');
+})
+
+// client buld-> build폴더->server/build->서버를 실행
+
+//client 폴더로 이동
+// npm ci
+//npm run build
+//client/build-> server/build폴더로 이동
+
+//server 폴더로 이동
+//npm ci
+//tsc
+//node app.js
